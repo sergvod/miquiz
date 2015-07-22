03c57b7b2125cccbad86801e8b7b343c0bc0ce57
sergvod <sergvod@yahoo.es>
2015-07-23 00:11:04 +0100

    Créditos incluidos

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET Créditos. */
+router.get('/author', function(req, res) {
+  res.render('author');
+});
+
 router.get('/quizzes/question', quizController.question);
 router.get('/quizzes/answer', quizController.answer);
 
